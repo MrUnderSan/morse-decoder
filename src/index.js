52f40d754d394713771f5ab38ef83eb7dfb70f98
MrUnderSan <MrUnderSan@gmail.com>
2022-01-17 20:07:48 +0300

    feat: Morse-decoder task complete

--- src/index.js
+++ src/index.js
@@ -1,46 +1,74 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
 };
 
 function decode(expr) {
-    // write your solution here
+  let morseCodeArr = [];
+
+  for (let i = 0; i < expr.length; i += 10) {
+    let numberCodeStr = expr.slice(i, i + 10);
+
+    let morseCodeStr = "";
+
+    for (let x = 0; x < 10; x += 2) {
+      if (numberCodeStr.slice(x, x + 2) == 10) {
+        morseCodeStr = morseCodeStr + ".";
+      } else if (numberCodeStr.slice(x, x + 2) == 11) {
+        morseCodeStr = morseCodeStr + "-";
+      }
+    }
+
+    morseCodeArr.push(morseCodeStr);
+  }
+
+  let decodeString = "";
+
+  morseCodeArr.forEach((elem) => {
+    if (elem == "") {
+      decodeString += " ";
+    } else {
+      decodeString += MORSE_TABLE[elem];
+    }
+  });
+
+  return decodeString;
 }
 
 module.exports = {
-    decode
+  decode
 }
